27308721e0a9f6e6b8029e8191c6d74919452e62
fm852369741 <93235715+fm852369741@users.noreply.github.com>
2021-11-15 10:11:39 +0530

    v1.1.0 (LTS)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const image = new PictureJS.RawImage(`https://picsum.photos/${Math.floor(window.innerWidth/2)}`);
+const image = new PictureJS.RawImage(`https://picsum.photos/${Math.floor(window.innerWidth/2)}/${Math.floor(window.innerHeight)}`);
 
 image.loadImage('.container', image.loadLens, [[(window.innerHeight/2)*0.35,(window.innerHeight/2)*0.35]]);
 image.self.on('mouseover', image.lens.load);

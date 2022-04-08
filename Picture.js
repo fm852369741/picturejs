d109bf94cdee6accd14542e72e636990fe73a467
fm852369741 <93235715+fm852369741@users.noreply.github.com>
2022-04-08 14:21:16 +0530

    refresh

--- Picture.js
+++ Picture.js
@@ -1,92 +1,104 @@
 const PictureJS = (() => {
-    return {
-        RawImage: class {
-            constructor(URL) {
-                this.image = new Image();
-                this.URL = URL;
+   return {
+      RawImage: class {
+         constructor(URL) {
+            this.image = new Image();
+            this.URL = URL;
 
-                this.self = {
-                    on: (event, callback) => {
-                        this.self.canvas.addEventListener(event, callback);
-                    }
-                }
-                
-                this.lens = {
-                    load: () => {
-                        this.container.appendChild(this.lens.canvas);
-                    },
-                    quit: () => {
-                        this.container.removeChild(this.lens.canvas);
-                    }
-                }
+            this.self = {
+               on: (event, callback) => {
+                  this.self.canvas.addEventListener(event, callback);
+               }
             }
 
-            loadImage(container, callback, args) {
-                this.container = document.querySelector(container);
-                this.image.src = this.URL;
-                this.image.crossOrigin = "Anonymous";
-
-                const canvas = document.createElement('canvas');
-                const context = canvas.getContext('2d');
-
-                this.self.canvas = canvas;
-                this.self.context = context;
-
-                this.image.onload = () => {
-                    canvas.width = this.image.width;
-                    canvas.height = this.image.height;
-                    context.drawImage(this.image, 0, 0, canvas.width, canvas.height);
-
-                    this.imageDimensions = [this.image.width, this.image.height];
-                    this.container.appendChild(canvas);
-
-                    const [lensCanvas, lensContext] = callback(canvas, context, this.container, ...args);
-                    this.lens.canvas = lensCanvas;
-                    this.lens.context = lensContext;
-                }
+            this.lens = {
+               load: () => {
+                  this.container.appendChild(this.lens.canvas);
+               },
+               quit: () => {
+                  this.container.removeChild(this.lens.canvas);
+               }
             }
+         }
 
-            loadLens(imageSource, imageContext, container, dimensions) {
-                const canvas = document.createElement('canvas');
-                const context = canvas.getContext('2d');
+         refresh() {
+            this.container.innerHTML = "";
+            this.loadImage(this.container, this.onImageFinishLoading, this.onImageFinishLoadingArgs)
+         }
 
-                canvas.width = imageSource.width;
-                canvas.height = imageSource.height;
-                canvas.style.pointerEvents = "none";
+         loadImage(container, callback, args) {
+            if (typeof container === "string")
+               this.container = document.querySelector(container);
+            else
+               this.container = container
 
-                imageSource.addEventListener('mousemove', (e) => {
-                    context.clearRect(0, 0, canvas.width, canvas.height);
-                    context.fillStyle = "rgba(0,0,0,0.25)";
-                    context.fillRect(e.clientX - (dimensions[0] / 2), e.clientY - (dimensions[1] / 2), dimensions[0], dimensions[1]);
-                    PictureJS.Preview.setRectDimensions([e.clientX - (dimensions[0] / 2), e.clientY - (dimensions[1] / 2), dimensions[0], dimensions[1]], imageContext);
-                });
+            this.image.src = this.URL;
+            this.image.crossOrigin = "Anonymous";
 
-                container.appendChild(canvas);
+            this.onImageFinishLoading = callback;
+            this.onImageFinishLoadingArgs = args;
 
-                return [canvas, context];
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+
+            this.self.canvas = canvas;
+            this.self.context = context;
+
+            this.image.onload = () => {
+               canvas.width = this.image.width;
+               canvas.height = this.image.height;
+               context.drawImage(this.image, 0, 0, canvas.width, canvas.height);
+
+               this.imageDimensions = [this.image.width, this.image.height];
+               this.container.appendChild(canvas);
+
+               const [lensCanvas, lensContext] = callback(canvas, context, this.container, ...args);
+               this.lens.canvas = lensCanvas;
+               this.lens.context = lensContext;
             }
-        },
-        Preview: class {
-            static setRectDimensions(rectDimensions, imageContext, imageObject) {
-                this.rectDimensions = rectDimensions;
-                this.imageContext = imageContext;
-                this.updatePreview();
-            }
+         }
 
-            static output(container) {
-                this.container = document.querySelector(container);
-                this.canvas = document.createElement('canvas');
-                this.context = this.canvas.getContext('2d');
-                this.container.appendChild(this.canvas);
-            }
+         loadLens(imageSource, imageContext, container, dimensions) {
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
 
-            static updatePreview() {
-                const rectData = this.imageContext.getImageData(...this.rectDimensions);
-                this.canvas.width = rectData.width;
-                this.canvas.height = rectData.height;
-                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-                this.context.putImageData(rectData, 0, 0);
-            }
-        }
-    }
+            canvas.width = imageSource.width;
+            canvas.height = imageSource.height;
+            canvas.style.pointerEvents = "none";
+
+            imageSource.addEventListener('mousemove', (e) => {
+               context.clearRect(0, 0, canvas.width, canvas.height);
+               context.fillStyle = "rgba(0,0,0,0.25)";
+               context.fillRect(e.clientX - (dimensions[0] / 2), e.clientY - (dimensions[1] / 2), dimensions[0], dimensions[1]);
+               PictureJS.Preview.setRectDimensions([e.clientX - (dimensions[0] / 2), e.clientY - (dimensions[1] / 2), dimensions[0], dimensions[1]], imageContext);
+            });
+
+            container.appendChild(canvas);
+
+            return [canvas, context];
+         }
+      },
+      Preview: class {
+         static setRectDimensions(rectDimensions, imageContext, imageObject) {
+            this.rectDimensions = rectDimensions;
+            this.imageContext = imageContext;
+            this.updatePreview();
+         }
+
+         static output(container) {
+            this.container = document.querySelector(container);
+            this.canvas = document.createElement('canvas');
+            this.context = this.canvas.getContext('2d');
+            this.container.appendChild(this.canvas);
+         }
+
+         static updatePreview() {
+            const rectData = this.imageContext.getImageData(...this.rectDimensions);
+            this.canvas.width = rectData.width;
+            this.canvas.height = rectData.height;
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.context.putImageData(rectData, 0, 0);
+         }
+      }
+   }
 })();
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const image = new PictureJS.RawImage(`https://picsum.photos/${Math.floor(window.innerWidth/2)}/${Math.floor(window.innerHeight)}`);
 
 image.loadImage('.container', image.loadLens, [[(window.innerHeight/2)*0.5,(window.innerHeight/2)*0.5]]);
+
 image.self.on('mouseover', image.lens.load);
 image.self.on('mouseout', image.lens.quit);
 
